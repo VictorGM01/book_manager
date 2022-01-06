52d61dd86959f2a603a5cef80f6669416b93dbba
Victor G. Marques <victormarques8801@gmail.com>
2022-01-06 18:08:40 -0300

    Cria estrutura padrão para o projeto django

---
+++ gerenciador_de_livros/gerenciador_de_livros/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for gerenciador_de_livros project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'gerenciador_de_livros.settings')
+
+application = get_wsgi_application()
