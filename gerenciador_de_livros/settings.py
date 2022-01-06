59a96aaab6714f49bc8fb16f5f58a36c895152da
Victor G. Marques <victormarques8801@gmail.com>
2022-01-06 18:13:09 -0300

    Recria estrutura padrão para o projeto django

--- gerenciador_de_livros/settings.py
+++ gerenciador_de_livros/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-13#ede)#f+i%k3zn!%m-zrex^j+b8iu87=9t4^94a-zt)18+x5'
+SECRET_KEY = 'django-insecure-&q8o+n)&bjdw4uu^4vf7ufur4@95*$b7)uwo==53bx431u2tn='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
