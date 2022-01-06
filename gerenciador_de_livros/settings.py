ef5969537086200d4b0053ab6229e6e47fad6699
Victor G. Marques <victormarques8801@gmail.com>
2022-01-06 18:28:45 -0300

    configurando .settings

--- gerenciador_de_livros/settings.py
+++ gerenciador_de_livros/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from key.key_django import KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&q8o+n)&bjdw4uu^4vf7ufur4@95*$b7)uwo==53bx431u2tn='
+SECRET_KEY = KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
