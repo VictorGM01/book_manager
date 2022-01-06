ba5976833c74cd7bc26c9f7f4593024e867f1ea0
Victor G. Marques <victormarques8801@gmail.com>
2022-01-06 19:14:35 -0300

    Adiciona pasta templates e static, configurando-as para receberem o html e os arquivos estáticos futuros

--- gerenciador_de_livros/settings.py
+++ gerenciador_de_livros/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 from key.key_django import KEY
 
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'gerenciador_de_livros.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'app_gerenciador_de_livros/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,10 +114,13 @@ USE_I18N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'gerenciador_de_livros/static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
