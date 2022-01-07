b11fc34c0716ae666defb4b92fc519190407821e
Victor G. Marques <victormarques8801@gmail.com>
2022-01-07 20:09:40 -0300

    Adiciona modelo em models.py para enviá-los ao banco de dados

--- app_gerenciador_de_livros/models.py
+++ app_gerenciador_de_livros/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Livros(models.Model):
+    nome_do_livro = models.CharField(max_length=200)
+    editora_do_livro = models.CharField(max_length=200)
+    autor = models.CharField(max_length=200)
+    genero = models.CharField(max_length=200)
+    estrelas = models.IntegerField()
+    situacao = models.CharField(blank=True)
+    opiniao = models.TextField(blank=True)
