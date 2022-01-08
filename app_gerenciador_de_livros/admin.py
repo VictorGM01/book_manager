c7e29ad80d938f640af5055045ff8ff1fe4ffbe2
Victor G. Marques <victormarques8801@gmail.com>
2022-01-07 22:09:06 -0300

    Adiciona item na página do admin-django

--- app_gerenciador_de_livros/admin.py
+++ app_gerenciador_de_livros/admin.py
@@ -3,7 +3,7 @@ from .models import Livros
 
 
 class ListaLivros(admin.ModelAdmin):
-    list_display = ('nome_do_livro', 'autor', 'estrelas', 'editora_do_livro', 'id')
+    list_display = ('nome_do_livro', 'autor', 'estrelas', 'editora_do_livro', 'situacao', 'id')
     list_display_links = ('nome_do_livro', 'id')
     search_fields = ('nome_do_livro', 'generos')
     list_filter = ('autor', 'estrelas', 'generos')
