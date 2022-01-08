01c707bfb1f08bf73cf8ba471a16a45e44c5b9d4
Victor G. Marques <victormarques8801@gmail.com>
2022-01-07 22:06:59 -0300

    Alteração no objeto 'estrelas': Possibilidade de ser nulo

--- app_gerenciador_de_livros/models.py
+++ app_gerenciador_de_livros/models.py
@@ -6,6 +6,6 @@ class Livros(models.Model):
     editora_do_livro = models.CharField(max_length=200)
     autor = models.CharField(max_length=200)
     generos = models.CharField(max_length=200)
-    estrelas = models.IntegerField()
+    estrelas = models.IntegerField(blank=True, null=True)
     situacao = models.CharField(max_length=100, blank=True)
     opiniao = models.TextField(max_length=500, blank=True)
