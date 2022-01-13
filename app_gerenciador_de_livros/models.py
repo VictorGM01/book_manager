e1b11793dd78b8c6673074cbdaba24cc9fbe3c5f
VictorGM01 <victormarques8801@gmail.com>
2022-01-13 17:51:08 -0300

    Cria modelo StatusLivro

--- app_gerenciador_de_livros/models.py
+++ app_gerenciador_de_livros/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 
 
+class StatusLivro(models.Model):
+    status = models.TextField(max_length=10)
+
+    def __str__(self):
+        return self.status
+
+
 class Livros(models.Model):
     nome_do_livro = models.CharField(max_length=200)
     editora_do_livro = models.CharField(max_length=200)
