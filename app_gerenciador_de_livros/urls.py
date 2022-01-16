d1c4c20e09bec87deabbb101caeff03b56142bb0
VictorGM01 <victormarques8801@gmail.com>
2022-01-16 18:47:53 -0300

    Referencia url dos arquivos de mídia em urls.py

--- app_gerenciador_de_livros/urls.py
+++ app_gerenciador_de_livros/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('buscar', views.buscar, name='buscar')
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
