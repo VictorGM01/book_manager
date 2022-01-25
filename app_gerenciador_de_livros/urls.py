81c875a43154341fbd002f4681ce223c8016f40c
VictorGM01 <victormarques8801@gmail.com>
2022-01-25 16:26:18 -0300

    Adiciona ID do livro na url de editar

--- app_gerenciador_de_livros/urls.py
+++ app_gerenciador_de_livros/urls.py
@@ -9,5 +9,5 @@ urlpatterns = [
     path('<int:id_livro>', views.livro, name='livro'),
     path('marcar-como-lido/<int:id_livro>', views.marcar_como_lido, name='marcar_como_lido'),
     path('adiciona/livro', views.adiciona_livro, name='add_livro'),
-    path('edita_livro', views.edita_livro, name='edita_livro')
+    path('edita/<int:id_livro>', views.edita_livro, name='edita_livro')
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
