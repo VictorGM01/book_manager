863b39427ea495dd37d0cca59b6a56927d24770b
VictorGM01 <victormarques8801@gmail.com>
2022-02-02 14:15:34 -0300

    Configura secrect_key utilizando arquivo settings.ini

--- gerenciador_de_livros/settings.py
+++ gerenciador_de_livros/settings.py
@@ -1,10 +1,8 @@
 import os
+from decouple import config
 from pathlib import Path
 import dj_database_url
 import django_heroku
-import environ
-
-environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -14,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['secret_key']
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
